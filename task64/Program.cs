435dc974f1c8d6ae538025b8109cd55668b00376
Irik Kamaldinov <irik.frost@mail.ru>
2023-03-30 20:59:28 +0300

    добавила папку tasl64 и task68

---
+++ task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+
+int m = InputInt("Введите положительное число: M= ");
+int n = InputInt("Введите положительное число: N= ");
+
+if (n < m)
+{
+    Console.WriteLine("Ввели отрицательное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
